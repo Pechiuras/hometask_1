348b099201eb3e8a271853685832a9d5b1bb5393
Aleksandr Pechiuras <alpechura@yandex.ru>
2023-01-26 21:37:47 +0500

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Введите 1 - ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите 2 - ое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+if (n > m)
+Console.WriteLine($" Число: {n} больше числа {m}");
+else if (m > n)
+Console.WriteLine($" Число: {m} больше числа {n}");
+
+// else (n == m);
+// Console.WriteLine($" Число: {m} равно {n}");
+
+ 
