8cb9f3f65c8634fd9ac3d185f7ca933ea4632d74
Aleksandr Pechiuras <alpechura@yandex.ru>
2023-01-27 11:24:42 +0500

    Задача 4 выполнена

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4: вывод наибольшего числа из трех
+Console.Clear();
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+  if (a > c)
+  {
+    Console.WriteLine($" Маскимальное число:  {a}");
+  }
+  else 
+  {
+   Console.WriteLine($" Маскимальное число:  {c}");
+  }
+}
+else if (b > c)
+{
+   Console.WriteLine($" Маскимальное число:  {b}");
+}
+else
+{
+Console.WriteLine($" Маскимальное число:  {c}");
+}
+
+ 
