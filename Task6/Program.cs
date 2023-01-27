03f005f532933868266d559d6cf966e403c39522
Aleksandr Pechiuras <alpechura@yandex.ru>
2023-01-27 11:45:50 +0500

    Задача 6 выполнена

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 6: определение четного и нечетного числа:
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n % 2 == 1)
+{
+    Console.Write("Число нечетное.");
+}
+else
+{
+    Console.Write("Число четное");
+}
