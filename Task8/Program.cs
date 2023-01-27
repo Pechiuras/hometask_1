0e43f32b793bbfedc2494957f16494517daf6dd1
Aleksandr Pechiuras <alpechura@yandex.ru>
2023-01-27 12:48:11 +0500

    Задача выполнена

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 8: Вывод четных чисел:
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 2; i < n; i += 2)
+Console.WriteLine($"{i} ");
